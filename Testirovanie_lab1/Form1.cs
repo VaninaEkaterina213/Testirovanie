dd11191de4a46c6ad6b49ca87d9b5ecbfc32f6f5
EkaVan <113054106+VaninaEkaterina213@users.noreply.github.com>
2023-10-16 13:46:24 +0300

    Add files via upload

---
+++ Testirovanie_lab1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testirovanie_lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            bool a1 = long.TryParse(textBox1.Text, out long a);
+            bool b1 = long.TryParse(textBox2.Text, out long b);
+            bool c1 = long.TryParse(textBox3.Text, out long c);
+                if ((a1 && b1 && c1) == true)
+                {
+                    if (a < 1 || b < 1 || c < 1)
+                    {
+                        MessageBox.Show("Число должно быть больше 0!");
+                    }
+                    else
+                    {
+                        if ((c >= a && c >= b && a + b > c) || (b >= a && b >= c && a + c > b) || (a >= b && a >= c && b + c > a))
+                        {
+                            if (a == b && a == c)
+                            {
+                                MessageBox.Show("Треугольник равносторонний");
+                            }
+                            else if (a == b || a == c || b == c)
+                            {
+                                MessageBox.Show("Треугольник равнобедренный");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Треугольник разносторонний");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Это не треугольник");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Данные не корректны или число слишком большое");
+                }
+            }
+        }
+    }
+
